90563e107e7f565b834f676705810fd4892e899d
Nikeweke <Driver3@meta.ua>
2018-06-25 10:12:48 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@
 *
 *  Работа с БД (все подключение сохраняем в "conn" - (Example: global.databases.mysql.conn = ...)
 */
+const colors = require('colors')
 const dbConfig = global.config.databases
 
 module.exports = {
@@ -32,13 +33,15 @@ module.exports = {
   |--------------------------------------------------------------------------
   */
   connectMysql () {
+    let {host, user, password, database} = global.config.databases.mysql
     const Mysql = require('sync-mysql')
     dbConfig.mysql.conn = new Mysql({
-      host: 'localhost',
-      user: 'root',
-      password: 'db_pass',
-      database: 'db_name'
+      host,
+      user,
+      password,
+      database
     })
+    console.log(colors.green.bold('MySQL') + ' => Connected')
   },
 
   /*
@@ -47,16 +50,21 @@ module.exports = {
   |--------------------------------------------------------------------------
   */
   connectSqlite () {
-    dbConfig.sqlite.conn = require('sqlite-sync').connect('./db.sqlite')
+    let {db_adress} = global.config.databases.sqlite
+    dbConfig.sqlite.conn = require('sqlite-sync').connect(db_adress)
+    console.log(colors.green.bold('SQLite') + ' => Connected')
   },
 
   /*
   |--------------------------------------------------------------------------
   | Подключение к Mongodb
   |--------------------------------------------------------------------------
+  |
   */
   connectMongodb () {
-    dbConfig.mongodb.conn = require('mongoose').connect('mongodb://localhost:27017/ToDoDb')
+    let {db_adress} = global.config.databases.mongodb
+    dbConfig.mongodb.conn = require('mongoose').connect(db_adress)
+    console.log(colors.green.bold('MongoDB') + ' => Connected')
   }
 
 }
